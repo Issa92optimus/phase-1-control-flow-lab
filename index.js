2c35625d8d805b3faa4176a1ef78f9e42ef728fb
Issa Nyale <me@email.com>
2022-12-06 15:25:42 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someFeet){
+
+  if (someFeet<=400) {
+  print = 'This one is on me!';
+  }else if(someFeet >= 2000 && someFeet <= 2500){
+  print = 'I will gladly take your thirty bucks.';
+  }else if(someFeet > 2500){
+  print = 'No can do.'
+  }
+  console.log(someFeet)
+  return print
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return someCity === 'NYC'? 'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(passengerTip){
+  switch (passengerTip){
+    case 'generous':
+      return 'Thank you so much.'
+
+      case 'not as generous':
+      return 'Thank you.'
+
+      case 'thanks for everything':
+        return 'Bye.'
+      }
+    }
